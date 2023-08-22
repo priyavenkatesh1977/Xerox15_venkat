5155d54fedcd4bf69bd553bc592ca7e69199c472
priyavenkatesh1977 <venkatesh.bharathan@gmail.com>
2023-08-22 12:13:05 +0530

    initial commit

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,84 @@
+/**
+ * This sevice contains functions used to log traces and exception to azure environment
+ * 
+ */
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AngularPlugin } from '@microsoft/applicationinsights-angularplugin-js';
+import { ApplicationInsights, Exception } from '@microsoft/applicationinsights-web';
+import { Router } from '@angular/router';
+
+import { StorageService } from '../services/storage.service';
+import {environment} from '../../environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+
+  private storageProvider: Storage;
+  env = environment;
+  private angularPlugin = new AngularPlugin();
+
+
+  private appInsights = new ApplicationInsights({
+    config: {
+        instrumentationKey: this.env.instrumentationKey,
+        enableAutoRouteTracking: true
+    },
+  }); 
+
+  constructor(
+    private http: HttpClient,
+    private storageService: StorageService,
+    private router: Router
+    ) {
+    this.storageProvider = this.storageService.getLocalStorage(true);
+    this.appInsights.loadAppInsights();
+     
+    this.appInsights.trackPageView();
+  }
+
+// expose methods that can be used in components and services
+trackEvent(name: string): void {
+  this.appInsights.trackEvent({ name });
+}
+
+trackTrace(message: string): void {
+  this.appInsights.trackTrace({ message });
+}
+
+trackException(exception : Exception){
+  this.appInsights.trackException(exception);
+} 
+
+public logMsg(message: string, logType?: string): void {
+
+  const config = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': 'ED803572-7B6B-4E56-8DCB-9F9C22C679FA'
+    })
+  };
+
+    const deviceID = this.storageProvider.getItem('deviceId');
+
+    const argParms = {
+      LogMessage: message,
+      LogType: logType || LogTypes.Information,
+      DeviceID: deviceID
+    };
+
+    this.http.post(this.env.deviceUrl+':5155/api/log', argParms, config).subscribe();
+  }
+  
+}
+
+export const LogTypes = {
+  Information: 'information',
+  Error: 'error',
+  Warning: 'warning'
+};
+
---
+++ src/app/views/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,93 @@
+
+/**
+ * Privacy Policy Component
+ *
+ * Description: This component is used to show the privacy policy
+ *
+ * Usage:
+ * <app-privacy-policy></app-privacy-policy>
+ *
+ *
+ * Outputs:
+ * - A pop up will be shown with privacy policy.
+ *
+ */
+import { Component,OnInit,ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { ModalService} from '../../services/modal.service';
+import { ResourcestringService} from '../../services/resourcestring.service';
+import { LogService } from '../../services/log.service';
+
+import { environment } from '../../../environments/environment';
+import { resourceString} from '../../model/global';
+import { ProgressAlertComponent } from '../progress-alert/progress-alert.component';
+
+
+
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.less']
+})
+export class PrivacyPolicyComponent implements OnInit {
+
+  privacyPolicy : string = '';
+  showVersion: string = '';
+  env = environment;
+  resourceString : resourceString[];
+
+  constructor(
+    private http: HttpClient,
+    private modalService : ModalService,
+    public modalRef : MatDialogRef<any>,
+    private resourceStringService : ResourcestringService,
+    private  logService: LogService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private sanitizer: DomSanitizer
+    ){}
+
+  ngOnInit(): void {
+    
+    const progress =  this.modalService.openModalWithoutClose(ProgressAlertComponent,'','');
+    const url = this.env.privacyPolicyUrl;
+       
+    this.http.get(url, {responseType:'text'})
+      .subscribe({
+          next:(response) => {
+          this.privacyPolicy = (response as string);
+          //this.showVersion = this.resourceString["VERSION"];
+          progress.close();
+        },
+        error:(error) => {
+          this.logService.trackTrace("inside privacy policy error"+error);
+          this.showVersion = 'v1.0'; //this.strings.VERSION
+          progress.close();
+          //this.modalService.showGeneralError(error);
+        }
+    });
+
+    
+    
+    }
+    
+    ngAfterViewInit(): void {
+      this.changeDetectorRef.detectChanges(); // Trigger change detection
+  
+      setTimeout(() => {
+        this.modalService.emitViewVisible(); // Call the emitViewVisible method after a delay
+      });
+    }
+  
+    closeModal():void{
+      this.modalService.closeModal(this.modalRef);
+      document.querySelector('.cdk-overlay-backdrop-showing').classList.add('cdk-overlay-backdrop-hide');
+    }
+}
+
+  
+
+
